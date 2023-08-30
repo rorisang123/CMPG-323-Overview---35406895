b0f4ee78331c080bc2d5f042d7e2607fa57c91f7
rorisang123 <roriiisang@gmail.com>
2023-08-30 20:32:52 +0200

    added correct models and controllers

---
+++ Proj2redo/Models/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proj2redo.Models
+{
+    public class OrderDetails
+    {
+        public int OrderDetailsID { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+    }
+}
